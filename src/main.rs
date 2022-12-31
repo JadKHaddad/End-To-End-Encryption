4eb7838719a23264fb8b26d7f5bd0423a69a61f3
JadKHaddad <jadkhaddad@gmail.com>
2022-12-31 12:31:30 +0100

    todo: n, g and k

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::{collections::HashMap, sync::Arc};
+
 use futures_util::{SinkExt, StreamExt};
+use parking_lot::RwLock;
 use poem::{
     endpoint::StaticFilesEndpoint,
     get, handler,
@@ -12,31 +15,45 @@ use poem::{
 
 #[handler]
 fn ws(
-    Path(name): Path<String>,
+    Path((from, to)): Path<(String, String)>,
     ws: WebSocket,
-    sender: Data<&tokio::sync::broadcast::Sender<String>>,
+    clients: Data<&Arc<RwLock<HashMap<String, tokio::sync::mpsc::UnboundedSender<String>>>>>,
 ) -> impl IntoResponse {
-    let sender = sender.clone();
-    let mut receiver = sender.subscribe();
+    let clients = clients.clone();
+    let clients_recv = clients.clone();
+    let from_recv = from.clone();
+
+    let (sender, mut receiver) = tokio::sync::mpsc::unbounded_channel::<String>();
+
+    {
+        let mut clients_g = clients.write();
+        clients_g.insert(from.clone(), sender);
+    }
+
     ws.on_upgrade(move |socket| async move {
         let (mut sink, mut stream) = socket.split();
 
         tokio::spawn(async move {
             while let Some(Ok(msg)) = stream.next().await {
                 if let Message::Text(text) = msg {
-                    if sender.send(format!("{}: {}", name, text)).is_err() {
-                        break;
+                    println!("{} -> {}: {}", from, to, text);
+                    if let Some(buddy_sender) = clients.read().get(&to) {
+                        if buddy_sender.send(text).is_err() {
+                            break;
+                        }
                     }
                 }
             }
         });
 
         tokio::spawn(async move {
-            while let Ok(msg) = receiver.recv().await {
+            while let Some(msg) = receiver.recv().await {
                 if sink.send(Message::Text(msg)).await.is_err() {
                     break;
                 }
             }
+            //remove client
+            clients_recv.write().remove(&from_recv);
         });
     })
 }
@@ -48,11 +65,10 @@ async fn main() -> Result<(), std::io::Error> {
     }
     tracing_subscriber::fmt::init();
 
+    let clients: Arc<RwLock<HashMap<String, tokio::sync::mpsc::UnboundedSender<String>>>> =
+        Arc::new(RwLock::new(HashMap::new()));
     let app = Route::new()
-        .at(
-            "/ws/:name",
-            get(ws.data(tokio::sync::broadcast::channel::<String>(32).0)),
-        )
+        .at("/ws/:from/:to", get(ws.data(clients)))
         .nest(
             "/",
             StaticFilesEndpoint::new(std::path::Path::new(".").join("html"))
